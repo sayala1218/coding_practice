adee7b98542fb5a07509767e54ec82db0708655b
Stephany Ayala-Cerna <ayalacerna03@gmail.com>
2024-12-30 09:45:51 -0800

    add testbench to add16 for simulation

--- SystemVerilog_Practice/Adder/add16.sv
+++ SystemVerilog_Practice/Adder/add16.sv
@@ -13,3 +13,19 @@ module add16 (
     assign cout = total_logic[16];
 
 endmodule
+
+module add16_testbench();
+    input [15:0] a,
+    input [15:0] b,
+    input cin,
+    output [15:0] sum,
+    output cout
+
+    add16 dut (.*);
+
+    initial begin
+        a = 16'd0; b = 16'd4; #10;
+        a = 16'd5; b = 16'd5; #10;
+        a = 16'd10; b = 16'd1; #10;
+    end
+endmodule
